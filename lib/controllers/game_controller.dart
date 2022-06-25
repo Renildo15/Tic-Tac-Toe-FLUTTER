0a5479baf454b8cb687f0930eddf03deee85e89d
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-06-25 11:12:10 -0300

    Correção de bugs

--- lib/controllers/game_controller.dart
+++ lib/controllers/game_controller.dart
@@ -22,23 +22,24 @@ class GameController{
   GameController(){
     _initialize();
   }
-  void _initialize(){
+  void _initialize() {
     movesPlayer1.clear();
     movesPlayer2.clear();
     currentPlayer = PlayerType.player1;
     isSinglePlayer = false;
-    tiles = List<BoardTile>.generate(BOARD_SIZE, (index) => BoardTile(index + 1));
+    tiles =
+        List<BoardTile>.generate(BOARD_SIZE, (index) => BoardTile(index + 1));
   }
 
   void reset(){
     _initialize();
   }
 
-  void markBoardTileByIndex(index){
+  void markBoardTileByIndex(index) {
     final tile = tiles[index];
-    if(currentPlayer == PlayerType.player1) {
+    if (currentPlayer == PlayerType.player1) {
       _markBoardTileWithPlayer1(tile);
-    }else{
+    } else {
       _markBoardTileWithPlayer2(tile);
     }
 
@@ -55,12 +56,12 @@ class GameController{
   void _markBoardTileWithPlayer2(BoardTile tile) {
     tile.symbol = PLAYER2_SYMBOL;
     tile.color = PLAYER2_COLOR;
-    movesPlayer1.add(tile.id);
+    movesPlayer2.add(tile.id);
     currentPlayer = PlayerType.player1;
   }
 
   bool _checkPlayerWinner(List<int> moves) {
-    return winnerRules.any((rule)=>
+    return winnerRules.any((rule) =>
         moves.contains(rule[0]) &&
         moves.contains(rule[1]) &&
         moves.contains(rule[2]));
@@ -72,7 +73,7 @@ class GameController{
     return WinnerType.none;
   }
 
-  int automaticMove(){
+  int automaticMove() {
     var list = new List.generate(9, (i) => i + 1);
     list.removeWhere((element) => movesPlayer1.contains(element));
     list.removeWhere((element) => movesPlayer2.contains(element));
