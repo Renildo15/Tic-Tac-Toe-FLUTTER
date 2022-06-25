70b6fa31628172c8ee5488bb60f95b31061750d5
brenolves <brenoalves43@hotmail.com>
2022-06-25 20:57:38 -0300

    Observer implementado + outras mudanças #1

--- lib/models/board_tiles.dart
+++ lib/models/board_tiles.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:jogo_da_velha/models/observer.dart';
 
-class BoardTile{
+class BoardTile implements Observer {
   final int id;
   String symbol;
   Color color;
   bool enable;
 
-  BoardTile(
-    this.id,{
-      this.symbol = '',
-      this.color = Colors.black26,
-      this.enable = true
-    }
-  );
+  BoardTile(this.id,
+      {this.symbol = '', this.color = Colors.black26, this.enable = true});
+
+  @override
+  void update(String sym, Color col) {
+    symbol = sym;
+    color = col;
+  }
 }
